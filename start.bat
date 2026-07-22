8877fe9577d81a45c77c61cc4234c338566b515c
Abdul Muheed <abdulmuheed0011@gmail.com>
2026-07-22 13:37:57 +0530

    some change fix

--- start.bat
+++ start.bat
@@ -1,4 +1,15 @@
 @echo off
 cd /d e:\jobhunting\local-runner\local-runner
+
+REM Save the current branch name
+FOR /F "tokens=*" %%g IN ('git branch --show-current') do (SET CURRENT_BRANCH=%%g)
+
+REM Switch to main branch
+git checkout main
+
+REM Run the server
 python run.py
+
+REM Switch back to original branch when the server is stopped
+git checkout %CURRENT_BRANCH%
 pause
